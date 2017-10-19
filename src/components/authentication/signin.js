3be1a3352c4f0c05665719a533c85ac2bca23f56
Mauricio Mercado <mau@maumercado.com>
2017-10-18 21:35:32 -0700

    Add redux form to project
    Setup signin component with redux form
    
    Setup reducer with redux form

---
+++ src/components/authentication/signin.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+
+class Signin extends Component {
+    render() {
+        return (
+            <form>
+                <fieldset className="form-group">
+                    <label>Email</label>
+                    <input className="form-control" />
+                </fieldset>
+                <fieldset>
+                    <label>Password</label>
+                    <input className="form-control" />
+                </fieldset>
+                <button action="submit" className="btn btn-primary">
+                    Sign In
+                </button>
+            </form>
+        );
+    }
+}
+
+export default reduxForm({
+    form: 'signin',
+    fields: ['email', 'password']
+})(Signin);
