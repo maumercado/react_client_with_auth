52bde711030d096ce9235d057f9445c4b2c6fa3b
Mauricio Mercado <mau@maumercado.com>
2017-10-18 21:16:31 -0700

    Created Header and imported it on App Component

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Header from './header';
 
 class App extends Component {
     render() {
         return (
             <div>
+                <Header />
                 <h3>React app starter</h3>
             </div>
         );
