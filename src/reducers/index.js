a0ae8757654079e29da69c755e3037939e2ec25b
Mauricio Mercado <mau@maumercado.com>
2017-10-18 23:56:19 -0700

    Forgot to add index of reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
+import authenticationReducer from './authentication_reducer';
 
 const rootReducer = combineReducers({
-    form
+    form,
+    auth: authenticationReducer
 });
 
 export default rootReducer;
